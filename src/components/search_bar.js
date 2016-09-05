e6f9d8d4f1353e7fd1ea1b3d58235fee4bd2b406
Chandan Kumar <chandan316@outlook.com>
2016-09-06 03:50:08 +0530

    my version of youtube

---
+++ src/components/search_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+// create a class component which renders an input search field
+
+class SearchBar extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {term: ''};
+    }
+
+    render () {
+        return (
+            <div className="search-bar">
+                <input value={ this.state.term }
+                onChange={ event => this.handleInputChange(event.target.value) } />
+            </div>
+        );
+    }
+
+    handleInputChange (term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAKG8H87flsQkrgtihCG7xZQWL9TQlKduE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class YouTube extends Component {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('MKBHD');
+    }
+
+    videoSearch (term) {
+        YTSearch({key: API_KEY, term: term, maxResults: 10},(videos) => {
+                 this.setState({
+                     videos: videos,
+                     selectedVideo: videos[0]
+                 });
+             });
+    }
+
+    render () {
+
+        const debouncedVideoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => debouncedVideoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
+}
+
+ReactDom.render(<YouTube />, document.querySelector('.app'));
